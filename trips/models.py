2bc68c87dfac0317b799cb2991fa9a0ae6c03892
lucaguazzaroni <lucaguazzaroni@gmail.com>
2020-05-04 13:44:38 +0200

    add first simple models to app trips

--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Trip(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField("Trip's name", max_length=200) 
+	
+	def __str__(self):
+		return self.name
+
+
+class Passenger(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField("Passenger's name", max_length=200)
+	trip = models.ForeignKey("Trip", on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+
+class Stop(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField("Stop's name", max_length=200)
+	trip = models.ForeignKey("Trip", on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return self.name 
+
+
+class Assistant(models.Model):
+	id = models.AutoField(primary_key=True)
+	trip = models.ForeignKey("Trip", on_delete=models.CASCADE)
+	passenger = models.ForeignKey("Passenger", on_delete=models.CASCADE)
+	has_assisted = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.name 
+
+
