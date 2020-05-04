7248cffb5ac0477a24b693dd1e21f15a09be306a
lucaguazzaroni <lucaguazzaroni@gmail.com>
2020-05-04 13:51:01 +0200

    add first trip models to admin site

--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Trip)
+admin.site.register(models.Passenger)
+admin.site.register(models.Stop)
+admin.site.register(models.Assistant)
+
