f3091f275e52b876b299faaf2b3796bb3d9019ef
lucaguazzaroni <lucaguazzaroni@gmail.com>
2020-05-06 23:40:57 +0200

    add / to end of accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.account_login),
-    path('logout/', views.account_logout),
+    path('login', views.account_login),
+    path('logout', views.account_logout),
 ]
