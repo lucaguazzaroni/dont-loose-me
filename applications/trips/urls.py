f7a9c572fde3221f376d856c26e6e780ce12500e
lucaguazzaroni <lucaguazzaroni@gmail.com>
2020-05-11 14:23:23 +0200

    add endpoint for list of assistants

--- applications/trips/urls.py
+++ applications/trips/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create/', views.trip_create),
     path('passengers/<int:trip_id>/', views.passengers_managment),
     path('stops/<int:trip_id>/', views.trip_stops_list),
+    path('assistants/<int:stop_id>', views.assistants_managment),
 ]
