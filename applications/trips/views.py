c383a80c86efa54672724d1ffe9fcda253b4760c
lucaguazzaroni <lucaguazzaroni@gmail.com>
2020-05-11 14:14:43 +0200

    get assistants of stop

--- applications/trips/views.py
+++ applications/trips/views.py
@@ -1,5 +1,5 @@
 import json 
-
+from django.views.generic import TemplateView
 from django.db import transaction
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
@@ -15,6 +15,8 @@ from. serializers import (
 	AssistantSimpleSerializer  
 )
 
+class PruebaView(TemplateView):
+	template_name = 'trips/hello.html'
 
 #@login_required
 def trip_listing(request):
@@ -65,7 +67,14 @@ def trip_stops_list(request, trip_id):
 		return JsonResponse(serialized_data, safe=False, status=status.HTTP_200_OK)
 
 
-
+#@login_required
+@transaction.atomic
+def assistants_managment(request, stop_id):
+	if request.method == 'GET':
+		trip = Trip.objects.get(id=stop_id)
+		assistants = Assistant.objects.filter(trip=trip)
+		serialized_data = AssistantSimpleSerializer(instance=assistants, many=True).data
+		return JsonResponse(serialized_data, safe=False, status=status.HTTP_200_OK)
 
 
 
